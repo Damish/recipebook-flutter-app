722df5508460dbb5bc8ec07404ee265b3ce6feed
Damish <damishs88@gmail.com>
2021-06-15 20:38:13 +0530

    edit product ui update

--- lib/screens/edit_product.dart
+++ lib/screens/edit_product.dart
@@ -79,66 +79,271 @@ class _EditProductState extends State<EditProduct> {
               ? Text('Add Product')
               : Text('Edit Product')),
       body: Padding(
-        padding: const EdgeInsets.all(8.0),
+        padding: const EdgeInsets.all(15.0),
         child: ListView(
           children: <Widget>[
-            TextField(
-              controller: imageController,
-              decoration: InputDecoration(hintText: 'Product image'),
-              onChanged: (value) => productProvider.changeImage(value),
+            SizedBox(
+              height: 20.0,
             ),
-            TextField(
-              controller: recepieNameController,
-              decoration: InputDecoration(hintText: 'Product recepieName'),
-              onChanged: (value) => productProvider.changeRecepieName(value),
+            TextFormField(
+                controller: recepieNameController,
+                onChanged: (value) => productProvider.changeRecepieName(value),
+                // initialValue: recipe == null ? '' : recipe.title,
+                // validator: (String value) {
+                //   // if (value.trim().length <= 0) {
+                //   if (value.isEmpty || value.length < 5) {
+                //     return 'Title is required and should be 5+ characters long.';
+                //   }
+                // },
+                onSaved: (String value) {
+                  // _formData['foodName'] = value;
+                },
+                decoration: InputDecoration(
+                  hintText: 'Food Name',
+                  fillColor: Colors.grey[200],
+                  filled: true,
+                  border: new OutlineInputBorder(
+                      borderRadius: new BorderRadius.circular(25.7),
+                      borderSide:
+                          new BorderSide(width: 0.0, style: BorderStyle.none)),
+                  focusedBorder: OutlineInputBorder(
+                      borderRadius: new BorderRadius.circular(25.7),
+                      borderSide:
+                          new BorderSide(width: 0.0, style: BorderStyle.none)),
+                  contentPadding: EdgeInsets.fromLTRB(10.0, 10.0, 10.0, 0.0),
+                  prefixIcon: Padding(
+                    padding: const EdgeInsets.only(
+                        right: 15.0, left: 15, top: 15, bottom: 15),
+                    child: Icon(
+                      Icons.title_outlined,
+                      color: Colors.black,
+                    ),
+                  ),
+                )),
+            SizedBox(
+              height: 10.0,
             ),
-            TextField(
-              controller: authorController,
-              decoration: InputDecoration(hintText: 'Product author'),
-              onChanged: (value) => productProvider.changeAuthor(value),
+            TextFormField(
+                controller: imageController,
+                onChanged: (value) => productProvider.changeImage(value),
+                // initialValue: recipe == null ? '' : recipe.description,
+                // validator: (String value) {
+                //   // if (value.trim().length <= 0) {
+                //   if (value.isEmpty || value.length < 4) {
+                //     return 'Author is required and should be 4+ characters long.';
+                //   }
+                // },
+
+                onSaved: (String value) {
+                  // _formData['author'] = value;
+                },
+                decoration: InputDecoration(
+                  hintText: 'Image url',
+                  fillColor: Colors.grey[200],
+                  filled: true,
+                  border: new OutlineInputBorder(
+                      borderRadius: new BorderRadius.circular(25.7),
+                      borderSide:
+                          new BorderSide(width: 0.0, style: BorderStyle.none)),
+                  focusedBorder: OutlineInputBorder(
+                      borderRadius: new BorderRadius.circular(25.7),
+                      borderSide:
+                          new BorderSide(width: 0.0, style: BorderStyle.none)),
+                  contentPadding: EdgeInsets.fromLTRB(10.0, 10.0, 10.0, 0.0),
+                  prefixIcon: Padding(
+                    padding: const EdgeInsets.only(
+                        right: 15.0, left: 15, top: 15, bottom: 15),
+                    child: Icon(
+                      Icons.link,
+                      color: Colors.black,
+                    ),
+                  ),
+                )),
+            SizedBox(
+              height: 10.0,
             ),
-            TextField(
-              controller: ingredientsController,
-              decoration: InputDecoration(hintText: 'Product ingredients'),
-              onChanged: (value) => productProvider.changeIngredients(value),
+            TextFormField(
+                controller: authorController,
+                onChanged: (value) => productProvider.changeAuthor(value),
+                // initialValue: recipe == null ? '' : recipe.description,
+                // validator: (String value) {
+                //   // if (value.trim().length <= 0) {
+                //   if (value.isEmpty || value.length < 4) {
+                //     return 'Author is required and should be 4+ characters long.';
+                //   }
+                // },
+
+                onSaved: (String value) {
+                  // _formData['author'] = value;
+                },
+                decoration: InputDecoration(
+                  hintText: 'Author',
+                  fillColor: Colors.grey[200],
+                  filled: true,
+                  border: new OutlineInputBorder(
+                      borderRadius: new BorderRadius.circular(25.7),
+                      borderSide:
+                          new BorderSide(width: 0.0, style: BorderStyle.none)),
+                  focusedBorder: OutlineInputBorder(
+                      borderRadius: new BorderRadius.circular(25.7),
+                      borderSide:
+                          new BorderSide(width: 0.0, style: BorderStyle.none)),
+                  contentPadding: EdgeInsets.fromLTRB(10.0, 10.0, 10.0, 0.0),
+                  prefixIcon: Padding(
+                    padding: const EdgeInsets.only(
+                        right: 15.0, left: 15, top: 15, bottom: 15),
+                    child: Icon(
+                      Icons.person_outline,
+                      color: Colors.black,
+                    ),
+                  ),
+                )),
+            SizedBox(
+              height: 10.0,
             ),
-            TextField(
-              controller: aboutRecepieController,
-              decoration: InputDecoration(hintText: 'Product aboutRecepie'),
-              onChanged: (value) => productProvider.changeAboutRecepie(value),
+            TextFormField(
+                controller: ingredientsController,
+                onChanged: (value) => productProvider.changeIngredients(value),
+                // maxLines: 5,
+                keyboardType: TextInputType.multiline,
+                // initialValue: recipe == null ? '' : recipe.description,
+                // validator: (String value) {
+                //   // if (value.trim().length <= 0) {
+                //   if (value.isEmpty || value.length < 4) {
+                //     return 'Ingredients is required and should be 4+ characters long.';
+                //   }
+                // },
+                onSaved: (String value) {
+                  // _formData['ingredients'] = value;
+                },
+                decoration: InputDecoration(
+                  hintText: 'Ingredients',
+                  fillColor: Colors.grey[200],
+                  filled: true,
+                  border: new OutlineInputBorder(
+                      borderRadius: new BorderRadius.circular(25.7),
+                      borderSide:
+                          new BorderSide(width: 0.0, style: BorderStyle.none)),
+                  focusedBorder: OutlineInputBorder(
+                      borderRadius: new BorderRadius.circular(25.7),
+                      borderSide:
+                          new BorderSide(width: 0.0, style: BorderStyle.none)),
+                  contentPadding: EdgeInsets.fromLTRB(10.0, 10.0, 10.0, 0.0),
+                  prefixIcon: Padding(
+                    padding: const EdgeInsets.only(
+                        right: 15.0, left: 15, top: 15, bottom: 15),
+                    child: Icon(
+                      Icons.food_bank_outlined,
+                      color: Colors.black,
+                    ),
+                  ),
+                )),
+            SizedBox(
+              height: 10.0,
             ),
-            TextField(
-              controller: cookingMethodController,
-              decoration: InputDecoration(hintText: 'Product cookingMethod'),
-              onChanged: (value) => productProvider.changeCookingMethod(value),
+            TextFormField(
+                controller: aboutRecepieController,
+                onChanged: (value) => productProvider.changeAboutRecepie(value),
+                // maxLines: 5,
+                // initialValue: recipe == null ? '' : recipe.description,
+                // validator: (String value) {
+                //   // if (value.trim().length <= 0) {
+                //   if (value.isEmpty || value.length < 4) {
+                //     return 'AboutRecipe is required and should be 4+ characters long.';
+                //   }
+                // },
+                onSaved: (String value) {
+                  // _formData['aboutRecipe'] = value;
+                },
+                decoration: InputDecoration(
+                  hintText: 'About Recipe',
+                  fillColor: Colors.grey[200],
+                  filled: true,
+                  border: new OutlineInputBorder(
+                      borderRadius: new BorderRadius.circular(25.7),
+                      borderSide:
+                          new BorderSide(width: 0.0, style: BorderStyle.none)),
+                  focusedBorder: OutlineInputBorder(
+                      borderRadius: new BorderRadius.circular(25.7),
+                      borderSide:
+                          new BorderSide(width: 0.0, style: BorderStyle.none)),
+                  contentPadding: EdgeInsets.fromLTRB(10.0, 10.0, 10.0, 0.0),
+                  prefixIcon: Padding(
+                    padding: const EdgeInsets.only(
+                        right: 15.0, left: 15, top: 15, bottom: 15),
+                    child: Icon(
+                      Icons.receipt_long_outlined,
+                      color: Colors.black,
+                    ),
+                  ),
+                )),
+            SizedBox(
+              height: 10.0,
             ),
+            TextFormField(
+                controller: cookingMethodController,
+                onChanged: (value) =>
+                    productProvider.changeCookingMethod(value),
+                // maxLines: 5,
+                // initialValue: recipe == null ? '' : recipe.description,
+                // validator: (String value) {
+                //   // if (value.trim().length <= 0) {
+                //   if (value.isEmpty || value.length < 4) {
+                //     return 'Cooking Method is required and should be 4+ characters long.';
+                //   }
+                // },
+                onSaved: (String value) {
+                  // _formData['cookingMethod'] = value;
+                },
+                decoration: InputDecoration(
+                  hintText: 'Cooking Method',
+                  fillColor: Colors.grey[200],
+                  filled: true,
+                  border: new OutlineInputBorder(
+                      borderRadius: new BorderRadius.circular(25.7),
+                      borderSide:
+                          new BorderSide(width: 0.0, style: BorderStyle.none)),
+                  focusedBorder: OutlineInputBorder(
+                      borderRadius: new BorderRadius.circular(25.7),
+                      borderSide:
+                          new BorderSide(width: 0.0, style: BorderStyle.none)),
+                  contentPadding: EdgeInsets.fromLTRB(10.0, 10.0, 10.0, 0.0),
+                  prefixIcon: Padding(
+                    padding: const EdgeInsets.only(
+                        right: 15.0, left: 15, top: 15, bottom: 15),
+                    child: Icon(
+                      Icons.list,
+                      color: Colors.black,
+                    ),
+                  ),
+                )),
             SizedBox(
               height: 20.0,
             ),
             RaisedButton(
-              child: Text('Save'),
-              onPressed: () => showDialog<String>(
-                context: context,
-                builder: (BuildContext context) => AlertDialog(
-                  title: const Text('Save'),
-                  content: const Text('Save changes?'),
-                  actions: <Widget>[
-                    TextButton(
-                      onPressed: () {
-                        productProvider.saveProduct();
-                        Navigator.of(context).push(MaterialPageRoute(
-                            builder: (context) => HomeScreen()));
-                      },
-                      child: const Text('Save'),
-                    ),
-                    TextButton(
-                      onPressed: () => Navigator.pop(context, 'Cancel'),
-                      child: const Text('Cancel'),
-                    ),
-                  ],
-                ),
-              ),
-            ),
+                color: Colors.blue,
+                textColor: Colors.white,
+                child: Text('Save'),
+                onPressed: () => showDialog<String>(
+                    context: context,
+                    builder: (BuildContext context) => AlertDialog(
+                          title: const Text('Save'),
+                          content: const Text('Save changes?'),
+                          actions: <Widget>[
+                            TextButton(
+                              onPressed: () {
+                                productProvider.saveProduct();
+                                Navigator.of(context).push(MaterialPageRoute(
+                                    builder: (context) => HomeScreen()));
+                              },
+                              child: const Text('Save'),
+                            ),
+                            TextButton(
+                              onPressed: () => Navigator.pop(context, 'Cancel'),
+                              child: const Text('Cancel'),
+                            ),
+                          ],
+                        ))),
             (widget.product != null)
                 ? RaisedButton(
                     color: Colors.red,
